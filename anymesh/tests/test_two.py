73f1c57b199add7b7405902cf3f649e47358f539
Dave Paul <davepaul0@gmail.com>
2014-11-02 14:30:29 -0600

    initial reorganization

---
+++ anymesh/tests/test_two.py
@@ -0,0 +1,51 @@
+from anymesh.core import AnyMesh, AnyMeshDelegateProtocol
+
+
+import unittest
+
+class TestAnyMeshBasic(unittest.TestCase):
+
+    class LeftDelegate(AnyMeshDelegateProtocol):
+        def connected_to(self, device_info):
+            pass
+        def disconnected_from(self, name):
+            pass
+        def received_msg(self, message):
+            pass
+
+    class RightDelegate(AnyMeshDelegateProtocol):
+        def connected_to(self, device_info):
+            pass
+        def disconnected_from(self, name):
+            pass
+        def received_msg(self, message):
+            pass
+
+    def test_connect(self):
+        self.leftMesh = AnyMesh('left', ['stuff', 'things'], TestAnyMeshBasic.LeftDelegate(), )
+
+
+
+'''
+    def test_shuffle(self):
+        # make sure the shuffled sequence does not lose any elements
+        random.shuffle(self.seq)
+        self.seq.sort()
+        self.assertEqual(self.seq, range(10))
+
+        # should raise an exception for an immutable sequence
+        self.assertRaises(TypeError, random.shuffle, (1,2,3))
+
+    def test_choice(self):
+        element = random.choice(self.seq)
+        self.assertTrue(element in self.seq)
+
+    def test_sample(self):
+        with self.assertRaises(ValueError):
+            random.sample(self.seq, 20)
+        for element in random.sample(self.seq, 5):
+            self.assertTrue(element in self.seq)
+            '''
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='anymesh',
+      version='0.4.0',
+      description='AnyMesh is a multi-platform, decentralized, auto-discovery, auto-connect mesh networking API.',
+      url='https://github.com/AnyMesh/anyMesh-Python.git',
+      author='Dave Paul',
+      author_email='davepaul0@gmail.com',
+      license='MIT',
+      packages=['anymesh'],
+      zip_safe=False)
