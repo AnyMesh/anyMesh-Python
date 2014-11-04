9260da824c6c37d075bc15daf0fee224b48aff32
Dave Paul <davepaul0@gmail.com>
2014-11-03 20:53:42 -0600

    timeout option on reactor tester

--- anymesh/tests/reactortester.py
+++ anymesh/tests/reactortester.py
@@ -1,10 +1,21 @@
 import unittest
-from twisted.internet import reactor
+from twisted.internet import reactor, task
 
 class ReactorTestCase(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(ReactorTestCase, self).__init__(*args, **kwargs)
         self.done = False
+
+    def timeOutAfter(self, seconds):
+        f = reactor.callLater(seconds, self.timedOut)
+        f.start(seconds)
+
+    def timedOut(self):
+        self.done = True
+        self.assertTrue(False, "test timed out!")
+        print "Test Timed Out!"
+        if reactor.running:
+            reactor.stop()
 
     def reactorAssert(self, success, msg):
         if not self.done:
